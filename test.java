62e416feab90b88ae1066400725c529ca4405836
jgagan95 <38185924+jgagan95@users.noreply.github.com>
2018-04-11 16:47:16 -0700

    Update test.java

--- test.java
+++ test.java
@@ -1,2 +1,3 @@
-int[] rainfall = new int[7];
-double[] rainTotal = new double[7];
+// #1
+int[] rainfall = new int[7]; // how data would recorded if exact decimals were not taken
+double[] rainTotal = new double[7]; //how the data would be held if precise measurements were required
