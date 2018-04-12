89b83064d506fda1b3a727609ccdcef49b72fc2c
jgagan95 <38185924+jgagan95@users.noreply.github.com>
2018-04-11 17:07:44 -0700

    Update test.java

--- test.java
+++ test.java
@@ -1,3 +1,10 @@
 // #1
+// Question: Declare an array to hold data for the rainfall for each of the seven days of the week.
 int[] rainfall = new int[7]; // how data would recorded if exact decimals were not taken
 double[] rainTotal = new double[7]; //how the data would be held if precise measurements were required
+
+//#2
+// Question: Given (int[] table  = new int[3];)
+// How long is the array and what is the valid range of indicies?
+
+The array is 4 units long and the valid range of indicies is 0-2
